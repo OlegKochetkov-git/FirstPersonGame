c906d464df398f0df4158d15b2f47b04db7fd7b5
Oleg Kochetkov <olegkochetkov1@gmail.com>
2023-05-03 23:55:15 +0500

    Init

---
+++ Source/FirstPersonGame/FirstPersonGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstPersonGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSONGAME_API AFirstPersonGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
